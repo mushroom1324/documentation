ce13df3815a7639e31f9798b2f3f39a83f245235
Jen Gilbert <jen.gilbert@datadoghq.com>
2024-11-06 10:56:02 -0600

    Switch to a vertical allowlist

--- markdoc/console/app/src/components/builder/NewPrefForm.tsx
+++ markdoc/console/app/src/components/builder/NewPrefForm.tsx
@@ -3,6 +3,7 @@ import { CreatedPref } from './types';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
+import StepContent from '@mui/material/StepContent';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -107,7 +108,7 @@ function PrefIdSelector(props: {
 
 function PrefBuilder(props: { pref: CreatedPref; allowlist: DbData['allowlist'] }) {
   const { pref, allowlist } = props;
-  const steps = ['Choose the pref', 'Name the option set', 'Add options'];
+  const steps = ['Choose the pref', 'Choose the options'];
 
   const [activeStep, setActiveStep] = React.useState(0);
 
@@ -125,48 +126,38 @@ function PrefBuilder(props: { pref: CreatedPref; allowlist: DbData['allowlist'] 
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps: { completed?: boolean } = {};
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          {/* The contents of each step */}
-          {activeStep === 0 && (
-            <PrefIdSelector pref={pref} onCompleted={handleNext} allowlist={allowlist} />
-          )}
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((step, index) => (
+          <Step key={step}>
+            <StepLabel
+              optional={
+                index === steps.length - 1 ? (
+                  <Typography variant="caption">Last step</Typography>
+                ) : null
+              }
             >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
+              {step}
+            </StepLabel>
+            <StepContent>
+              {activeStep === 0 && (
+                <PrefIdSelector
+                  pref={pref}
+                  onCompleted={handleNext}
+                  allowlist={allowlist}
+                />
+              )}
+              <Box sx={{ mb: 2 }}>
+                <Button variant="contained" onClick={handleNext} sx={{ mt: 1, mr: 1 }}>
+                  {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                </Button>
+                <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+                  Back
+                </Button>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
     </Box>
   );
 }
